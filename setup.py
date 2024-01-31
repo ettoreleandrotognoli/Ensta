78365b00a825eec09e0aa060171dd6cda60fdebc
Ettore Leandro Tognoli <ettoreleandrotognoli@gmail.com>
2024-01-31 16:31:29 -0300

    add pyquery

--- setup.py
+++ setup.py
@@ -44,7 +44,8 @@ setup(
         "moviepy",
         "cryptography",
         "pyotp",
-        "ntplib"
+        "pyquery",
+        "ntplib",
     ],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
